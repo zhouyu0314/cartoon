1010eca87da6968c4dc3614135fcd61e6b0f7c7d
zhouyu0314 <181332969@qq.com>
2020-08-19 16:39:07 +0800

    提供了查询消费记录的接口

--- com.cartoon.main/com.cartoon.user/src/main/java/com/cartoon/serivce/AccountRecordService.java
+++ com.cartoon.main/com.cartoon.user/src/main/java/com/cartoon/serivce/AccountRecordService.java
@@ -1,17 +1,17 @@
 package com.cartoon.serivce;
 
 import com.cartoon.entity.AccountRecord;
+import com.cartoon.util.PageUtil;
 
-import java.util.List;
+import java.util.Map;
 
 public interface AccountRecordService {
 
     /**
      * 查询账户记录
-     * @param id
-     * @param type 记录分类type（元宝（gold）1、阅读券(coupon)2、积分(score)3、月票(ticket)4、vip5）
+     * 记录分类type（元宝（gold）1、阅读券(coupon)2、积分(score)3、月票(ticket)4、vip5）
      * @return
      */
-    List<AccountRecord> findAccountRecord(String id,Integer type);
+    PageUtil<AccountRecord> findAccountRecord(Map<String,Object> params);
 
 }
